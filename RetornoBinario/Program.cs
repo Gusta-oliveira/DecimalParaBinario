a9dc1ce4bb3662ffa8467e6eb94a44dedd48f02b
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-07 01:13:52 -0300

    Add project files.

---
+++ RetornoBinario/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.AccessControl;
+
+int numero, coef, restodiv;
+int[] bin = new int[8];
+Console.Write("Informe o número para converter em binário: ");
+numero = int.Parse(Console.ReadLine());
+
+retornoBinario(numero);
+int[] retornoBinario(int numero)
+{
+    for(int count = 0; count < bin.Length; count++)
+    {
+        bin[count] = numero % 2;
+        numero = numero / 2;
+    }
+    return bin;
+}
+for(int count = bin.Length-1; count >= 0; count--)
+{
+    Console.Write(bin[count]);
+}
